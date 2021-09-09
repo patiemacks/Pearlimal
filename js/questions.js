f4b5f6ef99de0b0c75ebfcb243e92cb31ab230a6
Patient Ndlovu <patiemacks@yahoo.com>
2021-09-09 18:12:58 +0200

    A kids animal knowledge game
    Kids animal sound knowledge timed quiz application

---
+++ js/questions.js
@@ -0,0 +1,112 @@
+let questions = [
+  {
+    numb: 1,
+    question: "A cow?",
+    answer: "Moos",
+    options: [
+      "Bark",
+      "Moos",
+      "Mews",
+      "Quacks"
+    ]
+  },
+  {
+    numb: 2,
+    question: "A donkey?",
+    answer: "Brays",
+    options: [
+      "Trumpets",
+      "Moos",
+      "Brays",
+      "Hisses"
+    ]
+  },
+  {
+    numb: 3,
+    question: "A duck?",
+    answer: "Quacks",
+    options: [
+      "Quacks",
+      "Whistles",
+      "Crows",
+      "Sings"
+    ]
+  },
+  {
+    numb: 4,
+    question: "A cat?",
+    answer: "Mews",
+    options: [
+      "Barks",
+      "Moos",
+      "Mews",
+      "Quacks"
+    ]
+  },
+  {
+    numb: 5,
+    question: "An elephant?",
+    answer: "Trumpets",
+    options: [
+      "Brays",
+      "Moos",
+      "Roars",
+      "Trumpets"
+    ]
+  },
+  {
+    numb: 6,
+    question: "A lion?",
+    answer: "Roars",
+    options: [
+      "Laughs",
+      "Moos",
+      "Roars",
+      "Brays"
+    ]
+  },
+  {
+    numb: 7,
+    question: "A dog?",
+    answer: "Barks",
+    options: [
+      "Barks",
+      "Moos",
+      "Mews",
+      "Brays"
+    ]
+  },
+  {
+    numb: 8,
+    question: "A cock?",
+    answer: "Crows",
+    options: [
+      "Quacks",
+      "Crows",
+      "Hisses",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 9,
+    question: "A lamb?",
+    answer: "Bleats",
+    options: [
+      "Bleats",
+      "Bellows",
+      "Mews",
+      "Howls"
+    ]
+  },
+  {
+    numb: 10,
+    question: "A frog?",
+    answer: "Croaks",
+    options: [
+      "Grunt",
+      "Moos",
+      "Croaks",
+      "Howl"
+    ]
+  }
+];
